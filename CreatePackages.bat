3bf19f36adad340c4a1a5f26d8332a4690c4b19d
romerod <daniel.romero@baseclass.ch>
2014-04-21 21:51:48 +0200

    Reset Baseclass.Contrib.Nuget.Linked to 1.0.2

--- CreatePackages.bat
+++ CreatePackages.bat
@@ -1,3 +1,3 @@
 .nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.nuspec -Version 1.0.3
 
-.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Linked\Baseclass.Contrib.Nuget.Linked.nuspec -Version 1.0.3
+.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Linked\Baseclass.Contrib.Nuget.Linked.nuspec -Version 1.0.2
