f5e929e030be9d04091abd54102829ed780e407f
romerod <daniel.romero@baseclass.ch>
2014-05-23 17:50:13 +0200

    Add new Baseclass.Contrib.Nuget.Output.Web package which adds publish msdeploy support for Baseclass.Contrib.Nuget.Output. fixes #3

--- CreatePackages.bat
+++ CreatePackages.bat
@@ -1,3 +1,5 @@
 .nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.nuspec -Version 1.0.4
 
+.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.Web.nuspec -Version 1.0.0
+
 .nuget\nuget.exe pack Baseclass.Contrib.Nuget.Linked\Baseclass.Contrib.Nuget.Linked.nuspec -Version 1.0.2
