7a1fcf59d7f613863bcbf01e5436b25f40f1cc14
Mickaël Nivet <mnivet@talentsoft.com>
2015-04-17 11:22:06 +0200

    Update release notes and version numbers

--- CreatePackages.bat
+++ CreatePackages.bat
@@ -1,4 +1,4 @@
-.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.nuspec -Version 1.0.7
+.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.nuspec -Version 1.0.8
 
 .nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.Web.nuspec -Version 1.0.1
 
