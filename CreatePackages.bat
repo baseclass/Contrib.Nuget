64fdaaec436ac72765f2ff17bfab362f7bf11486
Mickaël Nivet <mnivet@talentsoft.com>
2015-04-23 16:26:12 +0200

    Merge Baseclass.Contrib.Nuget.Output.Web.targets in Baseclass.Contrib.Nuget.Output.targets to produce a single package. Having one single package will simplify usage as it was not obvious that we should install another package when developing a website using a package depending on the first one.

--- CreatePackages.bat
+++ CreatePackages.bat
@@ -1,6 +1,6 @@
-.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.nuspec -Version 1.0.8
+.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.nuspec -Version 2.0.0
 
-.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.Web.nuspec -Version 1.0.1
+.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.Web.nuspec -Version 2.0.0
 
 .nuget\nuget.exe pack Baseclass.Contrib.Nuget.Linked\Baseclass.Contrib.Nuget.Linked.nuspec -Version 1.0.2
 
