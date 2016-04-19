bba560f6274fd00199c635d894b48f54c791fc16
Daniel Romero <daniel.romero@baseclass.ch>
2016-04-19 21:52:55 +0200

    Bump versions

--- CreatePackages.bat
+++ CreatePackages.bat
@@ -1,7 +1,7 @@
-.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.nuspec -Version 2.1.0
+.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.nuspec -Version 2.1.1
 
 .nuget\nuget.exe pack Baseclass.Contrib.Nuget.Output\Baseclass.Contrib.Nuget.Output.Web.nuspec -Version 2.0.0
 
-.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Linked\Baseclass.Contrib.Nuget.Linked.nuspec -Version 1.0.2
+.nuget\nuget.exe pack Baseclass.Contrib.Nuget.Linked\Baseclass.Contrib.Nuget.Linked.nuspec -Version 1.0.3
 
 .nuget\nuget.exe pack Baseclass.Contrib.Nuget.GitIgnoreContent\Baseclass.Contrib.Nuget.GitIgnoreContent.nuspec -Version 0.0.2
